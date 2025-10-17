2da114432a01200f801015b39100926bd98a9048
Michael Rybka <merybka@gmail.com>
2025-10-17 11:22:41 -0400

    Remove unused imports

--- app/src/main/java/com/mikerybka/schemacafe/MainActivity.kt
+++ app/src/main/java/com/mikerybka/schemacafe/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -25,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mikerybka.schemacafe.ui.theme.SchemaCafeTheme
 
