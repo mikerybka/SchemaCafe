dbf2d4b55cc74bff054e40d18dd568f7fa3f20c1
Michael Rybka <merybka@gmail.com>
2025-10-27 12:30:18 -0400

    Add link to create new PAT

--- app/src/main/java/com/mikerybka/schemacafe/MainActivity.kt
+++ app/src/main/java/com/mikerybka/schemacafe/MainActivity.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -47,7 +49,16 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
@@ -139,11 +150,33 @@ fun App(dataStore: androidx.datastore.core.DataStore<androidx.datastore.preferen
                         .padding(16.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    OutlinedTextField(
-                        value = inputToken,
-                        onValueChange = { inputToken = it },
-                        label = { Text("GitHub API Key") }
-                    )
+                    Column {
+                        OutlinedTextField(
+                            value = inputToken,
+                            onValueChange = { inputToken = it },
+                            label = { Text("GitHub API Key") }
+                        )
+                        BasicText(
+                            text = buildAnnotatedString {
+                                withLink(
+                                    LinkAnnotation.Url(
+                                        url = "https://github.com/settings/personal-access-tokens/new",
+                                    )
+                                ) {
+                                    withStyle(
+                                        style = SpanStyle(
+                                            color = Color.Blue,
+                                            textDecoration = TextDecoration.Underline
+                                        )
+                                    ) {
+                                        append("Create a new one")
+                                    }
+                                }
+                            },
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+
                 }
             }
         }
